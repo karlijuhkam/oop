ee0b2bd5c4a2a1ba80903152721c05eed2b73921
karlijuhkam <karli.juhkam@khk.ee>
2018-01-19 09:50:08 +0200

    'projekti kaustade konstantide määramine'

---
+++ conf.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karli
+ * Date: 19.01.2018
+ * Time: 09:44
+ */
+
+//kaustade ja failide konstantsed nimetused
+define('MODEL_DIR','model/');
+define('VIEW_DIR','views/');
+define('CONTROL_DIR','controllers/');
