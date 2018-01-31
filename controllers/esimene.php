e3a9a39017bb248bc6efdfe63b194ee3c51dc4ae
karlijuhkam <karli.juhkam@khk.ee>
2018-01-31 09:50:10 +0200

    'valmistatud vajalikud controllerid'

---
+++ controllers/esimene.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karli
+ * Date: 31.01.2018
+ * Time: 09:49
+ */
+
+$mainTmpl->set('content', 'Esimene leht');
---
+++ controllers/teine.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karli
+ * Date: 31.01.2018
+ * Time: 09:49
+ */
+$mainTmpl->set('content', 'Teine leht');
