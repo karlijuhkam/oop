38ad4bc0502a739e3724806749fe7f93512bd241
karlijuhkam <karli.juhkam@khk.ee>
2018-02-02 10:39:27 +0200

    'eemaldatud testlinkide kuvamine'

--- model/linkobjects.php
+++ model/linkobjects.php
@@ -19,7 +19,7 @@ class linkobject extends http
     {
         parent::__construct();
         $this->baseLink = $this->protocol.HTTP_HOST.SCRIPT_NAME;
-        echo $this->baseLink;
+
     }
     // loome paarid kujul nimi=väärtus
     // ühendame need ka kokku nimi1=väärtus1&nimi2=väärtus2
@@ -28,7 +28,7 @@ class linkobject extends http
             $link = $link.$this->delim;
         }
         $link = $link.fixUrl($name).$this->eq.fixUrl($value);
-        echo $link.'<br />';
+
     }
     // loome täislingi põhilingi ja paaride kasutamisel
     function getLink($add = array()){
