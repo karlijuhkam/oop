e6d072ca5fec5173759d616fe85214e331d6fa2a
karlijuhkam <karli.juhkam@khk.ee>
2018-02-12 09:01:45 +0200

    'loodud vajalik controller logimise tegemiseks'

---
+++ controllers/login.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karli
+ * Date: 12.02.2018
+ * Time: 08:56
+ */
+
+//loome sisselogimise vormi objekti
+$loginForm = new template('login');
+//paneme paika vajalikud väärtused malli sisustamiseks
+$loginForm->set('kasutaja','Kasutajanimi');
+$loginForm->set('parool','Parool');
+$loginForm->set('nupp','Logi sisse!');
+//paneme väärtused malli
+$mainTmpl=set('content', $loginForm->parse());
